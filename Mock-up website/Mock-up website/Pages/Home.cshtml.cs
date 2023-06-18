2bc37e9478cf12220b46bfc78350483ac15c42f7
U2Ber202 <ducdhhe163444@fpt.edu.vn>
2023-06-18 17:46:41 +0700

    Testing

---
+++ Mock-up website/Mock-up website/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mock_up_website.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
